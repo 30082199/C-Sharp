cb9803b9fab1960d74857f73bd65465e336fe0c2
Cloudee29 <151775715+Cloudee29@users.noreply.github.com>
2023-11-23 15:13:24 +0800

    Update Form1.cs

--- .github/ISSUE_TEMPLATE/Astronomical Processsing/Astronomical Processsing/Form1.cs
+++ .github/ISSUE_TEMPLATE/Astronomical Processsing/Astronomical Processsing/Form1.cs
@@ -1,13 +1,5 @@
 
 
-/* Andrei Diamante, Harvey1, Sprint Number 1
-// Date: 23/11/2023
-// Version: 17.70
-// Astronomical Processing Sprint 1
-// the program generates a random 24 number which is stored in array and is able to be sorted using bubble sort and it can be 
-   searched with binary searched method it also has an edit button to edit existing items in the listbox by selecting and using the textbox.
-*/
-
 
 
 namespace Astronomical_Processsing
